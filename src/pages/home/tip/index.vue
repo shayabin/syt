bf01446f17adb2c5eeb293fade90b31b0e665d21
xuyang <2072393754@qq.com>
2024-08-19 21:50:37 +0800

    16完成

--- src/pages/home/tip/index.vue
+++ src/pages/home/tip/index.vue
@@ -51,28 +51,129 @@
       </div>
       <div class="content">
         <ul>
-            <li>神经内科</li>
-            <li>消化内科</li>
-            <li>呼吸内科</li>
-            <li>内科</li>
-            <li>神经外科</li>
-            <li>妇科</li>
-            <li>产科</li>
-            <li>儿科</li>
+          <li>神经内科</li>
+          <li>消化内科</li>
+          <li>呼吸内科</li>
+          <li>内科</li>
+          <li>神经外科</li>
+          <li>妇科</li>
+          <li>产科</li>
+          <li>儿科</li>
         </ul>
       </div>
     </div>
     <!-- 平台公告 -->
     <div class="notice">
-        <div class="header">
-            <div class="left">
-              xxxxxx
-
-            </div>
-            <div class="right">
-
-            </div>
+      <div class="header">
+        <div class="left">
+          <svg
+            style="width: 20px; height: 20px"
+            t="1724073309017"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="8196"
+            width="200"
+            height="200"
+          >
+            <path
+              d="M342 391.1h24.7v180.4H342z"
+              fill="#FFFFFF"
+              p-id="8197"
+            ></path>
+            <path
+              d="M404.6 711.5c-0.4 3.7-0.6 7.5-0.6 11.2v70c0 59.7 48.4 108 108 108 59.7 0 108-48.4 108-108v-70c0-3.8-0.2-7.5-0.6-11.2H404.6z m0 0"
+              fill="#CCE5F6"
+              p-id="8198"
+            ></path>
+            <path
+              d="M512 923.9c-72.3 0-131.1-58.8-131.1-131.1v-70c0-4.5 0.2-9.1 0.7-13.6 1.2-11.8 11.1-20.7 23-20.7h214.9c11.8 0 21.8 8.9 23 20.7 0.5 4.5 0.7 9.1 0.7 13.6v70c-0.1 72.3-58.9 131.1-131.2 131.1z m-84.9-189.3v58.1c0 46.8 38.1 84.9 84.9 84.9 46.8 0 84.9-38.1 84.9-84.9v-58.1H427.1z m148-477.2c-0.8 0-1.6 0-2.4-0.1-12.7-1.3-21.9-12.7-20.6-25.4 0.1-1.4 0.2-2.8 0.2-4.2v-41.1c0-22.2-18.1-40.3-40.3-40.3-22.2 0-40.3 18.1-40.3 40.3v41.1c0 1.4 0.1 2.8 0.2 4.2 1.3 12.7-7.9 24.1-20.6 25.4-12.7 1.3-24.1-7.9-25.4-20.6-0.3-3-0.5-6-0.5-9v-41.1c0-47.7 38.8-86.5 86.5-86.5s86.5 38.8 86.5 86.5v41.1c0 3-0.2 6-0.5 9-1.1 11.9-11.1 20.7-22.8 20.7z m0 0"
+              fill="#2197EF"
+              p-id="8199"
+            ></path>
+            <path
+              d="M756.7 616.9c-12.8 0-23.1-10.3-23.1-23.1V481.3c0-122.2-99.4-221.6-221.6-221.6-122.2 0-221.6 99.4-221.6 221.6v112.5c0 12.8-10.3 23.1-23.1 23.1s-23.1-10.3-23.1-23.1V481.3c0-147.7 120.2-267.8 267.8-267.8 147.7 0 267.9 120.2 267.9 267.8v112.5c0 12.8-10.4 23.1-23.2 23.1z m0 0"
+              fill="#2197EF"
+              p-id="8200"
+            ></path>
+            <path
+              d="M168.3 677h687.5v113.6H168.3z"
+              fill="#CCE5F6"
+              p-id="8201"
+            ></path>
+            <path
+              d="M855.7 813.7H168.3c-12.8 0-23.1-10.3-23.1-23.1V677c0-12.8 10.3-23.1 23.1-23.1h687.5c12.8 0 23.1 10.4 23.1 23.1v113.6c0 12.8-10.4 23.1-23.2 23.1z m-664.3-46.2h641.3v-67.4H191.4v67.4z m0 0"
+              fill="#2197EF"
+              p-id="8202"
+            ></path>
+          </svg>
+          <span>平台公告</span>
         </div>
+        <div class="right">
+          <span>全部</span>
+          <svg
+            style="width: 20px; height: 20px"
+            t="1724069229427"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="5783"
+            width="200"
+            height="200"
+          >
+            <path
+              d="M357.957 167.176l-49.463 48.973 294.308 296.327-296.782 293.831 49.044 49.381 346.239-342.809z"
+              fill="#2c2c2c"
+              p-id="5784"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div class="content">
+        <ul>
+          <li>关于延长北京大学国际医院放假的通知</li>
+          <li>北京中医药大学东方医院部分科室医生门诊医</li>
+          <li>武警总医院号源暂停更新通知</li>
+        </ul>
+      </div>
+    </div>
+    <!-- 停诊公告 -->
+    <div class="notice">
+      <div class="header">
+        <div class="left">
+          <svg style="width:20px;height:20px" t="1724075047884" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9216" width="200" height="200"><path d="M736 768h-448a32 32 0 0 1 0-64h448a32 32 0 0 1 0 64z m-44.8-415.552l-147.669333-147.669333a61.994667 61.994667 0 0 1-62.848 0l-147.669334 147.669333a32.554667 32.554667 0 0 1-46.037333-46.037333l161.578667-161.578667a63.68 63.68 0 0 1 127.104 0l161.578666 161.578667a32.554667 32.554667 0 0 1-46.037333 46.037333zM565.333333 512a32 32 0 0 1 0 64h-277.333333a32 32 0 0 1 0-64h277.333333z" fill="#EB9E52" p-id="9217"></path><path d="M853.333333 981.333333H170.666667a85.333333 85.333333 0 0 1-85.333334-85.333333V384a85.333333 85.333333 0 0 1 85.333334-85.333333h682.666666a85.333333 85.333333 0 0 1 85.333334 85.333333v512a85.333333 85.333333 0 0 1-85.333334 85.333333z m21.333334-576a42.666667 42.666667 0 0 0-42.666667-42.666666H192a42.666667 42.666667 0 0 0-42.666667 42.666666v469.333334a42.666667 42.666667 0 0 0 42.666667 42.666666h640a42.666667 42.666667 0 0 0 42.666667-42.666666V405.333333z" fill="#5C8EF3" p-id="9218"></path></svg>
+          <span>停诊公告</span>
+        </div>
+        <div class="right">
+          <span>全部</span>
+          <svg
+            style="width: 20px; height: 20px"
+            t="1724069229427"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="5783"
+            width="200"
+            height="200"
+          >
+            <path
+              d="M357.957 167.176l-49.463 48.973 294.308 296.327-296.782 293.831 49.044 49.381 346.239-342.809z"
+              fill="#2c2c2c"
+              p-id="5784"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div class="content">
+        <ul>
+          <li>中国人民解放军总医院第六医学中心(原海军总医院)呼吸内科门诊停诊公告</li>
+          <li>首都医科大学附属北京潞河医院老年医学科门诊停诊公告</li>
+          <li>中日友好医院中西医结合心内科门诊停诊公告</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template> 
@@ -92,7 +193,7 @@
         justify-content: center;
         align-items: center;
         span {
-            margin-left: 5px;
+          margin-left: 5px;
         }
       }
       .right {
@@ -102,16 +203,46 @@
       }
     }
     .content {
-        ul{
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            li{
-                width:40%;    
-                margin-top: 20px;
-                margin-left: 10px;
-            }
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        li {
+          width: 40%;
+          margin-top: 20px;
+          margin-left: 10px;
         }
+      }
+    }
+  }
+  .notice {
+    margin-top: 50px;
+    .header {
+      display: flex;
+      justify-content: space-between;
+      .left {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .right{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+    .content {
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+        li {
+          overflow:hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          margin-top: 20px;
+          margin-left: 10px;
+        }
+      }
     }
   }
 }
